3eee14e5fd014757be48c804778aba2c7028aa51
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2018-02-23 15:11:10 +0000

    Code cleaned up and all tests completing

--- index.js
+++ index.js
@@ -6,99 +6,93 @@ const axios = require('axios');
 
 module.exports = {
 
-  makeAppId() {
-    return Math.random().toString(36).substring(2, 7);
-  },
+    makeAppId() {
+        return Math.random().toString(36).substring(2, 7);
+    },
 
-  /**
-   * Get API response
-   * @param  {String} apiUrl - URL of the API to retrieve
-   * @return (dependent upon request)
-   */
-  getApi(apiUrl) {
-    if (!apiUrl) return 'No API URL supplied';
+    // @todo remove getApi()? Seems redundant
+    /**
+     * Get API response
+     * @return {Object} (dependent upon request)
+     */
+    getApi: function (apiUrl) {
+        if (!apiUrl) return 'No API URL supplied';
 
-    return https.get(apiUrl, res => {
-      res.setEncoding("utf8");
-      let body = "";
-      res.on("data", data => {
-        body += data;
-      })
-      ;
-      res.on("end", () => {
-        // console.log(res);
-        body = JSON.parse(body);
-        return body
-      });
-      res.on("error", () => {
-        console.log('Error');
-        // @todo return res?
-        return "There was an error"
-      })
-    })
-  },
+        return https.get(apiUrl, res => {
+            res.setEncoding("utf8");
+            let body = "";
+            res.on("data", data => {
+                body += data;
+            })
+            ;
+            res.on("end", () => {
+                body = JSON.parse(body);
+                return body
+            });
+            res.on("error", () => {
+                console.log('Error');
+                return "There was an error"
+            })
+        })
+    },
 
-  /**
-   * Get API response using Axios
-   * @param  {String} apiUrl - URL of the API to retrieve
-   * @return (dependent upon request)
-   */
-  getAxiosApi(apiUrl) {
-    if (!apiUrl) return 'No API URL supplied';
-    return axios.get(apiUrl)
-      .then(function (res) {
-        // console.log(response);
-        // res.data
-        return res
-      })
-      .catch(function (error) {
-        // console.log(error);
-        return error
-      });
-  },
+    /**
+     * Get API response using Axios
+     * @param  {String} apiUrl - URL of the API to retrieve
+     * @return {Object} (dependent upon request)
+     */
+    getAxiosApi(apiUrl) {
+        if (!apiUrl) return 'No API URL supplied';
+        return axios.get(apiUrl)
+            .then(function (res) {
+                return res
+            })
+            .catch(function (error) {
+                return error
+            });
+    },
 
-  /**
-   * Get upcoming artist events
-   * @param  {String} artistName - string of the artist ID (must be registered with BandsInTown)
-   * @param  {Stirng} appId (optional) - unique ID required for API call. If not provided, a random token will be generated
-   * @return {Object} JSON response
-   */
-  getArtistEvents(artistName, appId)
-  {
-    if (!artistName) return 'artistName is required';
-    if (!appId) return 'appId is required';
-    var apiUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=" + appId;
-    return this.getAxiosApi(apiUrl)
-  },
+    /**
+     * Get upcoming artist events
+     * @param  {String} artistName - string of the artist ID (must be registered with BandsInTown)
+     * @param  {String} appId (optional) - unique ID required for API call. If not provided, a random token will be generated
+     * @return {Object} JSON response
+     */
+    getArtistEvents(artistName, appId) {
+        if (!artistName) return 'artistName is required';
+        if (!appId) return 'appId is required';
+        let apiUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=" + appId;
+        return this.getAxiosApi(apiUrl)
+    },
 
 
-  mhs_bandsintown(artistName, appId)
-  {
-    if (artistName == null) return 'Artist ID is required';
-    appId = appId || this.makeAppId();
+    mhs_bandsintown: function (artistName, appId) {
+        if (artistName == null) return 'Artist ID is required';
+        appId = appId || this.makeAppId();
 
-    var theResponse = this.getArtistEvents(artistName, appId)
-    // if(theResponse.data === []) return 'No upcoming events'
-    return theResponse;
-    // @todo respond with error if unknown artist is supplied
-    // @todo return JSON if instructed
-    // @todo format options if successful
-    // @todo add 'options' [] param which includes formatting type, appId etc. mhs_bandsintown(artistId, options)
-    // @todo store transient?
-    // @todo handle 404 and 500 errors
+        let theResponse = null;
 
-    // console.log(res.response.data);
-    // if (res.response.data.errors[0]) {
-    //   return res.response.data.errors[0]
-    // } else {
-    //   return res.response.data
-    // }
+        return this.getArtistEvents(artistName, appId).then((res) => {
+            if (res.status === 200 && res.data) {
+                theResponse = res
+            } else if (res.response.status === 403) {
+                theResponse = 'Artist is not registered with BandsInTown'
+            }
+        }).then(() => {
+            return theResponse
+        });
 
-    // console.log(theResponse)
+        // @todo respond with error if unknown artist is supplied
+        // @todo return JSON if instructed
+        // @todo format options if successful
+        // @todo add 'options' [] param which includes formatting type, appId etc. mhs_bandsintown(artistId, options)
+        // @todo store transient?
+        // @todo handle 404 and 500 errors
 
-  }
+    }
 
 };
 // @todo format events function
 // @todo write readme.md file
 // @todo write basic tests
+// @todo capture errors for promises
--- test/index.test.js
+++ test/index.test.js
@@ -1,121 +1,119 @@
-const https        = require('https');
-const expect       = require('chai').expect;
-const nock         = require('nock');
+const expect = require('chai').expect;
+const nock = require('nock');
 const api_response = require('./assets/response');
-const testMe       = require('../index.js');
+const testMe = require('../index.js');
 
 describe('makeAppID', () => {
 
-  it('Should return a random 5 character string', () => {
-    expect(testMe.makeAppId()).to.be.string;
-    expect(testMe.makeAppId()).to.have.lengthOf(5);
-  });
+    it('Should return a random 5 character string', () => {
+        expect(typeof(testMe.makeAppId())).to.equal('string');
+        expect(testMe.makeAppId()).to.have.lengthOf(5);
+    });
 
 });
 
 describe('getApi', () => {
 
-  it('should terminate if the URL is not supplied', () => {
-    expect(testMe.getApi()).to.be.string;
-    expect(testMe.getApi()).to.equal('No API URL supplied');
-  });
+    it('should terminate if the URL is not supplied', () => {
+        expect(typeof(testMe.getApi())).to.equal('string');
+        expect(testMe.getApi()).to.equal('No API URL supplied');
+    });
 
 });
 
 describe('getAxiosApi', () => {
 
-  it('should terminate if the URL is not supplied', () => {
-    expect(testMe.getAxiosApi()).to.be.string;
-    expect(testMe.getAxiosApi()).to.equal('No API URL supplied');
-  });
+    it('should terminate if the URL is not supplied', () => {
+        expect(typeof(testMe.getAxiosApi())).to.equal('string');
+        expect(testMe.getAxiosApi()).to.equal('No API URL supplied');
+    });
 
 });
 
 describe('getArtistEvents', () => {
-  beforeEach(() => {
+    beforeEach(() => {
 
-  });
+    });
 
-  it('Should terminate if no artistID supplied', () => {
-    expect(testMe.getArtistEvents()).to.be.string;
-    expect(testMe.getArtistEvents()).to.equal('artistName is required');
-  });
+    it('Should terminate if no artistID supplied', () => {
+        expect(typeof(testMe.getArtistEvents())).to.equal('string');
+        expect(testMe.getArtistEvents()).to.equal('artistName is required');
+    });
 
-  it('Should terminate if no appId supplied', () => {
-    expect(testMe.getArtistEvents('A Wilhelm Scream')).to.be.string;
-    expect(testMe.getArtistEvents('A Wilhelm Scream')).to.equal('appId is required');
-  });
+    it('Should terminate if no appId supplied', () => {
+        expect(typeof(testMe.getArtistEvents('A Wilhelm Scream'))).to.equal('string');
+        expect(testMe.getArtistEvents('A Wilhelm Scream')).to.equal('appId is required');
+    });
 
-  it('Should return JSON object if successful call is made', () => {
-    nock('https://rest.bandsintown.com')
-      .get('/artists/A%20Wilhelm%20Scream/events?app_id=AWS')
-      .reply(200, api_response.hasEvents());
-    return testMe.getArtistEvents('A Wilhelm Scream', 'AWS')
-      .then(res => {
-        // console.log(res)
-        expect(res.status).to.equal(200);
-        expect(typeof res).to.equal('object');
-      })
-  });
+    it('Should return JSON object if successful call is made', () => {
+        nock('https://rest.bandsintown.com')
+            .get('/artists/A%20Wilhelm%20Scream/events?app_id=AWS')
+            .reply(200, api_response.hasEvents());
+        return testMe.getArtistEvents('A Wilhelm Scream', 'AWS')
+            .then(res => {
+                expect(res.status).to.equal(200);
+                expect(typeof res).to.equal('object');
+            })
+    });
 
-  it('Should return with 403 if unknown artist is supplied', () => {
-    nock('https://rest.bandsintown.com')
-      .get('/artists/234234/events?app_id=123')
-      .reply(403, api_response.unknownArtist());
-    return testMe.getArtistEvents('234234', '123')
-      .then(res => {
-        expect(res.response.status).to.equal(403);
-      })
-  });
+    it('Should return with 403 if unknown artist is supplied', () => {
+        nock('https://rest.bandsintown.com')
+            .get('/artists/234234/events?app_id=123')
+            .reply(403, api_response.unknownArtist());
+        return testMe.getArtistEvents('234234', '123')
+            .then(res => {
+                expect(res.response.status).to.equal(403);
+            })
+    });
 
-  it('Should return empty array if no upcoming events for artist', () => {
-    nock('https://rest.bandsintown.com')
-      .get('/artists/A%20Wilhelm%20Scream/events?app_id=AWS')
-      .reply(200, api_response.noEvents());
-    return testMe.getArtistEvents('A Wilhelm Scream', 'AWS')
-      .then(res => {
-        expect(res.status).to.equal(200);
-        expect(typeof res.data).to.equal('object');
-        expect(res.data.length).to.equal(0);
-      })
-  });
+    it('Should return empty array if no upcoming events for artist', () => {
+        nock('https://rest.bandsintown.com')
+            .get('/artists/A%20Wilhelm%20Scream/events?app_id=AWS')
+            .reply(200, api_response.noEvents());
+        return testMe.getArtistEvents('A Wilhelm Scream', 'AWS')
+            .then(res => {
+                expect(res.status).to.equal(200);
+                expect(typeof res.data).to.equal('object');
+                expect(res.data.length).to.equal(0);
+            })
+    });
 
 });
 
-// @todo write tests for mhs_bandsintown main function
 describe('mhs_bandsintown (main function)', () => {
 
-  beforeEach(()=> {
-    nock('https://rest.bandsintown.com')
-      .get('/artists/A%20Wilhelm%20Scream/events')
-      .query(true)
-      .reply(200, api_response.hasEvents());
-  });
+    beforeEach(() => {
+        nock('https://rest.bandsintown.com')
+            .get('/artists/A%20Wilhelm%20Scream/events')
+            .query(true)
+            .reply(200, api_response.hasEvents());
+    });
 
-  it('Should fail if no artistId is supplied', () => {
-    expect(testMe.mhs_bandsintown()).to.equal('Artist ID is required');
-  });
+    it('Should fail if no artistId is supplied', () => {
+        expect(testMe.mhs_bandsintown()).to.equal('Artist ID is required');
+    });
 
-  it('Should succeed if no appID is supplied - random string is generated', () => {
+    it('Should succeed if no appID is supplied - random string is generated', () => {
 
-    return testMe.mhs_bandsintown('A Wilhelm Scream').then((res)=> {
-      expect(typeof(res.data)).to.equal('object');
-      expect(res.data[0].hasOwnProperty('id')).to.be.true;
-      expect(res.data[0]['artist_id']).to.equal('2121');
+        return testMe.mhs_bandsintown('A Wilhelm Scream').then((res) => {
+            // console.log(res)
+            expect(typeof(res.data)).to.equal('object');
+            expect(res.data[0].hasOwnProperty('id')).to.be.true;
+            expect(res.data[0]['artist_id']).to.equal('2121');
+        })
+
+    });
+
+    it('Should return a string if unknown artistId is supplied', () => {
+        nock('https://rest.bandsintown.com')
+            .get('/artists/234234/events')
+            .query(true)
+            .reply(403, api_response.unknownArtist());
+
+        return testMe.mhs_bandsintown('234234').then((res) => {
+            expect(typeof(res)).to.equal('string');
+            expect(res).to.equal('Artist is not registered with BandsInTown');
+        })
+
     })
-  });
-
-  it('Should return a string if unknown artistId is supplied', () => {
-    nock('https://rest.bandsintown.com')
-      .get('/artists/234234/events')
-      .query(true)
-      .reply(403, api_response.unknownArtist());
-
-    return testMe.mhs_bandsintown('234234').then((res)=> {
-      console.log(res)
-      expect(typeof(res.data)).to.equal('string');
-
-    })
-
-  })
-})
+});
